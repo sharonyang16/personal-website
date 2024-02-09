a84d19403e49765acf7f6d2cc9a49be80ced5635
Sharon Yang <sharonyyang0@gmail.com>
2024-02-08 19:37:05 -0500

    #8 Moved grid formatting to seperate file

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <Main>{children}</Main>
+    </>
+  );
+}
---
+++ src/components/layout/Main.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Grid } from "@mui/material";
+
+export default function Main({ children }) {
+  return (
+    <main>
+      <Grid container spacing={2}>
+        <Grid item xs={3} />
+        <Grid item xs={9}>
+          {children}
+        </Grid>
+      </Grid>
+    </main>
+  );
+}
