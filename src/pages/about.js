d81ea192dbe89901cc45dc37fa3543ef8cc7aed0
Sharon Yang <sharonyyang0@gmail.com>
2024-02-28 12:48:15 -0500

    #32 Testing linting check (should fail)

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import PageHeading from '../components/PageHeading';
+import { Typography } from '@mui/material';
 
 export default function AboutPage() {
   return <PageHeading title='About' />;
