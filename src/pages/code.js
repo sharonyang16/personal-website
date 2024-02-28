c78ddf966f2b733a738c061c3e9c8b4d91290610
Sharon Yang <sharonyyang0@gmail.com>
2024-02-28 18:30:22 -0500

    #20 Linting

--- src/pages/code.js
+++ src/pages/code.js
@@ -7,8 +7,9 @@ export default function CodePage() {
     <>
       <PageHeading title='Code' />
       <Typography variant='subtitle1'>
-        Here are some projects I've worked on over the years. I particularly
-        like working on frontend related things.
+        {
+          "Here are some projects I've worked on over the years. I particularly like working on frontend related things."
+        }
       </Typography>
       <Divider />
     </>
