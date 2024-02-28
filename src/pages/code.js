bf8d9dcecfcb489acb3e6618f2904e13ab20cbb9
Sharon Yang <sharonyyang0@gmail.com>
2024-02-28 18:09:06 -0500

    #20 Added a caption for code page

--- src/pages/code.js
+++ src/pages/code.js
@@ -1,6 +1,16 @@
 import * as React from 'react';
 import PageHeading from '../components/PageHeading';
+import { Typography, Divider } from '@mui/material';
 
 export default function CodePage() {
-  return <PageHeading title='Code' />;
+  return (
+    <>
+      <PageHeading title='Code' />
+      <Typography variant='subtitle1'>
+        Here are some projects I've worked on over the years. I particularly
+        like working on frontend related things.
+      </Typography>
+      <Divider />
+    </>
+  );
 }
