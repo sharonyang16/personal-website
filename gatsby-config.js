a3b74b636c5f063f7d14a608319d67f43ad998f7
Sharon Yang <sharonyyang0@gmail.com>
2024-03-01 13:42:41 -0500

    #7 removed unnecessary comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /assets/, // See below to configure properly
+          include: /assets/,
         },
       },
     },
