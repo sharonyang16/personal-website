9fc96b074fc77b47387073e7c35a35f43e910d76
Sharon Yang <sharonyyang0@gmail.com>
2026-04-06 20:42:44 -0400

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import withMDX from "@next/mdx";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = withMDX({
+  extension: /\.mdx?$/,
+})({
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+});
 
 export default nextConfig;
