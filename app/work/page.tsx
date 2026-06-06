33062925d6c3f82f3bf94a3b9664dcbf33466919
Sharon Yang <sharonyyang0@gmail.com>
2026-06-05 20:33:30 -0400

    add work heading

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,13 +1,15 @@
 import {
   BrowseGrid,
   BrowseGridRow,
+  BrowseHeading,
 } from "@/components/layout/BrowseGrid/BrowseGrid";
-import { codeData, designData } from "./data";
+import { codeData, designData, pageDescription, pageTitle } from "./data";
 import { ProjectCard } from "@/components/cards/ProjectCard";
 
 export default function Work() {
   return (
     <BrowseGrid>
+      <BrowseHeading title={pageTitle} description={pageDescription} />
       <BrowseGridRow title="Code">
         {codeData.map((project) => (
           <ProjectCard key={project.title} {...project} />
